197054d3ffc389a1b72b1f46134bae19f9cd49e3
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-25 23:19:39 +0530

    added vans page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
 
 function App() {
 	return (
@@ -12,11 +13,13 @@ function App() {
 				</Link>
 				<nav>
 					<Link to='/about'>About</Link>
+					<Link to='/vans'>Vans</Link>
 				</nav>
 			</header>
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/about' element={<About />} />
+				<Route path='/vans' element={<Vans />} />
 			</Routes>
 		</>
 	);
