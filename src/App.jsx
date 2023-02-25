258c272628c39b8af36376385fb847350bc12e16
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-25 21:10:01 +0530

    home and about page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
-	return <div className='text-4xl'>Hello</div>;
+	return (
+		<>
+			<header>
+				<Link className='site-logo' to='/'>
+					#VanLife
+				</Link>
+				<nav>
+					<Link to='/about'>About</Link>
+				</nav>
+			</header>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/about' element={<About />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
