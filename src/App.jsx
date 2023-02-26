73af03790e529df0c5b26d551b2b0d09f0b418fa
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-26 23:07:56 +0530

    added outlet

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,18 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
 import VanDetails from "./pages/VanDetails";
+import Layout from "./components/Layout";
 
 function App() {
 	return (
 		<>
-			<header>
-				<Link className='site-logo' to='/'>
-					#VanLife
-				</Link>
-				<nav>
-					<Link to='/about'>About</Link>
-					<Link to='/vans'>Vans</Link>
-				</nav>
-			</header>
 			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/vans' element={<Vans />} />
-				<Route path='/vans/:id' element={<VanDetails />} />
+				<Route element={<Layout />}>
+					<Route path='/' element={<Home />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/vans' element={<Vans />} />
+					<Route path='/vans/:id' element={<VanDetails />} />
+				</Route>
 			</Routes>
 		</>
 	);
