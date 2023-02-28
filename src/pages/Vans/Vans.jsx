75baf149483f4b1a055ed7ce266542fc496fc934
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-28 23:47:47 +0530

    filter button highlights on selected

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -36,18 +36,29 @@ const Vans = () => {
 		<div className='van-list-container'>
 			<h1>Explore our van options</h1>
 			<div className='van-list-filter-buttons'>
-				<button onClick={() => setSearchParams({ type: "simple" })} className='van-type simple'>
+				<button
+					onClick={() => setSearchParams({ type: "simple" })}
+					className={`van-type simple ${typeFilter === "simple" && "selected"}`}
+				>
 					Simple
 				</button>
-				<button onClick={() => setSearchParams({ type: "luxury" })} className='van-type luxury'>
+				<button
+					onClick={() => setSearchParams({ type: "luxury" })}
+					className={`van-type luxury ${typeFilter === "luxury" && "selected"}`}
+				>
 					Luxury
 				</button>
-				<button onClick={() => setSearchParams({ type: "rugged" })} className='van-type rugged'>
+				<button
+					onClick={() => setSearchParams({ type: "rugged" })}
+					className={`van-type rugged ${typeFilter === "rugged" && "selected"}`}
+				>
 					Rugged
 				</button>
-				<button onClick={() => setSearchParams({})} className='van-type clear-filters'>
-					Clear filter
-				</button>
+				{typeFilter !== null && (
+					<button onClick={() => setSearchParams({})} className='van-type clear-filters'>
+						Clear filter
+					</button>
+				)}
 			</div>
 			<div className='van-list'>{vanElements}</div>
 		</div>
