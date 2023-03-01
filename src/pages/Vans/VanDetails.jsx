260d02cad9ea2cdaf6ff4186b9f11635b1ab3111
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-01 13:07:52 +0530

    added useLocation()

--- src/pages/Vans/VanDetails.jsx
+++ src/pages/Vans/VanDetails.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useLocation } from "react-router-dom";
 
 export default function VanDetail() {
 	const params = useParams();
+	const location = useLocation();
 	const [van, setVan] = React.useState(null);
 
 	React.useEffect(() => {
@@ -11,9 +12,11 @@ export default function VanDetail() {
 			.then((data) => setVan(data.vans));
 	}, [params.id]);
 
+	const search = location.state?.search || "";
+	console.log(search);
 	return (
 		<div className='van-detail-container'>
-			<Link to='..' relative='path' className='back-button'>
+			<Link to={`..${search}`} relative='path' className='back-button'>
 				&larr; <span>Back to all vans</span>
 			</Link>
 			{van ? (
