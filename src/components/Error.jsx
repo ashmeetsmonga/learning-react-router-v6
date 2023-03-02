8802735081b937f529c556feea9053cd86925e2d
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-02 15:06:04 +0530

    added error message in error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const Error = () => {
-	return <div>Hello, an error occured</div>;
+	const error = useRouteError();
+	return (
+		<>
+			<h1>Error: {error.message}</h1>
+			<pre>
+				{error.status} - {error.statusText}
+			</pre>
+		</>
+	);
 };
 
 export default Error;
