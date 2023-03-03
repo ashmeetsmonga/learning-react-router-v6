df44dc6ea7edee26105e19d43ee6f58ca5aff9be
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-03 23:23:44 +0530

    added login message

--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const AuthRequired = () => {
 	const auth = { token: null };
-	if (!auth.token) return <Navigate to='/login' />;
+	if (!auth.token) return <Navigate to='/login' state={{ message: "You must login first" }} />;
 	return <Outlet />;
 };
 
