0f6f5d5f1b39914b554ffcd5058fa1cab224a963
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-04 23:04:16 +0530

    added replace true in navigate in authRequired

--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -4,7 +4,8 @@ import { Navigate, Outlet } from "react-router-dom";
 const AuthRequired = () => {
 	const auth = localStorage.getItem("loggedIn");
 	console.log(auth);
-	if (auth !== "true") return <Navigate to='/login' state={{ message: "You must login first" }} />;
+	if (auth !== "true")
+		return <Navigate to='/login' replace state={{ message: "You must login first" }} />;
 	return <Outlet />;
 };
 
