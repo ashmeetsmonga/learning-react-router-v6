d33a890fd4194ced6f3a05f7f3d41e14fe273e9a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-05 23:15:09 +0530

    handled error from action in react-router-dom Form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,15 +6,17 @@ export async function action({ request }) {
 	const formData = await request.formData();
 	const email = formData.get("email");
 	const password = formData.get("password");
-	const data = await loginUser({ email, password });
-	localStorage.setItem("loggedIn", true);
-	console.log(localStorage.getItem("loggedIn"));
-	return data;
+	try {
+		const data = await loginUser({ email, password });
+		localStorage.setItem("loggedIn", true);
+		return data;
+	} catch (error) {
+		return { error: error.message };
+	}
 }
 
 export default function Login() {
 	const [status, setStatus] = useState("idle");
-	const [error, setError] = useState(null);
 	const data = useActionData();
 
 	const location = useLocation();
@@ -24,23 +26,23 @@ export default function Login() {
 	console.log(data);
 	if (data?.token) navigate(from, { replace: true });
 
-	function handleSubmit(e) {
-		setError(null);
-		e.preventDefault();
-		setStatus("submitting");
-		loginUser(loginFormData)
-			.then((data) => {
-				localStorage.setItem("loggedIn", true);
-				navigate(from, { replace: true });
-			})
-			.catch((error) => setError(error.message))
-			.finally(() => setStatus("idle"));
-	}
+	// function handleSubmit(e) {
+	// 	setError(null);
+	// 	e.preventDefault();
+	// 	setStatus("submitting");
+	// 	loginUser(loginFormData)
+	// 		.then((data) => {
+	// 			localStorage.setItem("loggedIn", true);
+	// 			navigate(from, { replace: true });
+	// 		})
+	// 		.catch((error) => setError(error.message))
+	// 		.finally(() => setStatus("idle"));
+	// }
 
 	return (
 		<div className='login-container'>
 			{location?.state?.message && <h2 className='login-error'>{location.state.message}</h2>}
-			{error && <h2 className='login-error'>{error}</h2>}
+			{data?.error && <h2 className='login-error'>{data?.error}</h2>}
 			<h1>Sign in to your account</h1>
 			<Form action='/login' method='post' className='login-form'>
 				<input name='email' type='email' placeholder='Email address' />
